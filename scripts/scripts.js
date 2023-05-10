fea848de77f8b7ee46dc5bfb3f75a8c6a4fe1bbe
AnthoEnge <anthony.engels@outlook.fr>
2023-05-10 03:07:43 +0200

    Terminer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,18 +25,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
-  document.getElementById("generateButton").addEventListener("click", async () => {
+  const button = document.getElementById("generateButton")
+  button.addEventListener("click", async () => {
     const subject = document.getElementById("subject").value;
     const keywords = document.getElementById("keywords").value;
+    const occasion = document.getElementById('occasion').value;
+    const chaleur = document.getElementById('chaleur').value;
 
-      const occasion = document.getElementById('occasion').value;
-      const chaleur = document.getElementById('chaleur').value;
-
-      
     if (!subject || !keywords) {
       alert("Veuillez entrer un sujet et des points clés.");
       return;
     }
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+  </svg>`;
+    button.style.opacity = 0.8 ;
+    button.disabled = true;
     let occasionText = "";
     switch (occasion) {
       case "anniversaire":
@@ -78,5 +83,11 @@ document.addEventListener("DOMContentLoaded", function () {
       // Afficher le discours généré dans la zone de texte
       const generatedSpeechTextarea = document.getElementById("generatedSpeech");
       generatedSpeechTextarea.value = generatedSpeech;
+      setTimeout(function() {
+        button.innerHTML = "Valider";
+        button.style.opacity = 1 ;
+        button.disabled = false;
+      }, 1000);
+
   });
 });
